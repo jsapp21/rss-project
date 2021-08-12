b5785d25e5b09b1dfab684b3ca0baaaf8730389a
James Sapp <jsapp@ucdavis.edu>
2021-08-12 11:51:06 -0700

    form(WIP)

--- client/src/App.js
+++ client/src/App.js
@@ -5,34 +5,47 @@ import Button from '@material-ui/core/Button';
 
 const App = () => {
 
-  const [data, setData] = useState({})
+  const [data, setData] = useState('')
+  const [form, setForm] = useState({ item: '', price: 0})
 
   useEffect(() => {
-    fetch('/express_backend')
+    fetch('/users')
     .then(res => res.json())
     .then(data => {
       console.log(data)
-      setData(data)
+      setData(data[0].name)
     })
   }, []);
 
-  const handleClick = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
 
+    console.log('i am clicked')
+    console.log(form)
     // POST FOOD ITEM TO MONGODB
   }
+
+  const handleChange = (e) => {
+    setForm((form) => ({
+      ...form,
+      [e.target.name]: e.target.value
+    }))
+  };
 
   return (
     <div className="m-8">
       <div className="">
         <header className="App-header">
-          <Typography variant="body1" component="span">{data.express}</Typography>
+          <Typography variant="body1" component="span">{data}'s name is coming from MongoDB!</Typography>
         </header>
       </div>
-          <Input defaultValue="Hello world" inputProps={{ 'aria-label': 'description' }} />
+      <form onSubmit={handleSubmit}>
+          <Input placeholder="Menu Item" inputProps={{ 'aria-label': 'item' }} name="item" value={form.item} onChange={handleChange}/>
+          <Input placeholder="$9.99" inputProps={{ 'aria-label': 'price' }} name="price" value={form.price} onChange={handleChange} />
           <div className="my-2">
-            <Button variant="outlined" onClick={handleClick}>Save</Button>
+            <Button type="submit" variant="contained" color="primary">Save</Button>
           </div>
+      </form>
     </div>
   );
 }
