5db344e2b4930a2a4ae7939090067e0af3bbd31a
James Sapp <jsapp@ucdavis.edu>
2021-08-12 11:49:32 -0700

    form(WIP)

--- server/server.js
+++ server/server.js
@@ -1,50 +1,44 @@
 const express = require('express');
+const mongoose= require('mongoose');
 const app = express(); 
 const port = process.env.PORT || 5000;
+const User = require('./models/user')
 
 const url = 'mongodb://127.0.0.1:27017'
 const dbName = 'point-of-sale'
 
-const mongoose= require('mongoose')
-// mongoose is playing off the mongodb libary 
+app.use(express.json());
 
-// connects to db
 mongoose.connect(`${url}/${dbName}`, {
   useNewUrlParser: true,
-  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+
+app.get('/')
+
+app.get('/users', (req, res) => {
+  User.find()
+  .then(users => res.send(users))
+  // res.send(req.body);
 })
 
-// create first model
-const User = mongoose.model('User', {
-  name: {
-    type: String
-  },
-  age: {
-    type: Number
-  }
-})
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
+}); 
 
-const me = new User({
-  name: 'James',
-  age: 'mike'
-})
 
-me.save()
-.then(() => {
-  console.log(me)
-})
-.catch((e) => {
-  console.log('Error', e)
-})
+app.listen(port, () => console.log(`Listening on port ${port}`)); 
+
+
+
+
+
 
 
 // const mongodb = require('mongodb');
 // const MongoClient = mongodb.MongoClient
 // const ObjectID = mongodb.ObjectId
-
-
-
-
 
 // MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
 //   if (err) {
@@ -63,11 +57,3 @@ me.save()
   // })
 // })
 
-app.get('/')
-
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
-}); 
-
-
-app.listen(port, () => console.log(`Listening on port ${port}`)); 
