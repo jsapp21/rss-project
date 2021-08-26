71d347b8b061e430e085518d48df4e468149d125
James Sapp <jsapp@ucdavis.edu>
2021-08-26 16:58:12 -0700

    refactor(error handling) removed try/catch

--- server/routes/items.routes.js
+++ server/routes/items.routes.js
@@ -5,28 +5,20 @@ const router = express.Router();
 const service = require('../services/items.service');
 
 router.post('/', async (req, res) => {
-  try {
-    const verifyItem = await service.getItemCheck(req.body);
-    if (verifyItem) {
-      res.send({ error: 'Item already exsits.' });
-    } else {
-      const newItem = await service.postMenuItem(req.body);
-      res.send(newItem);
-    }
-  } catch (e) {
-    console.log(e);
+  const verifyItem = await service.getItemCheck(req.body);
+  if (verifyItem) {
+    res.send({ error: 'Item already exsits.' });
+  } else {
+    const newItem = await service.postMenuItem(req.body);
+    res.send(newItem);
   }
 });
 
 router.get(['/', '/:id'], async (req, res) => {
-  try {
-    const menu = { menuId: req.params.id };
+  const menu = { menuId: req.params.id };
 
-    const menuItems = await service.getMenuItems(menu);
-    res.send(menuItems);
-  } catch (e) {
-    console.log(e);
-  }
+  const menuItems = await service.getMenuItems(menu);
+  res.send(menuItems);
 });
 
 module.exports = router;
--- server/services/items.service.js
+++ server/services/items.service.js
@@ -1,9 +1,9 @@
 const mongoService = require('./mongo.service');
 
 const service = {
-  getMenuItems: (user) => mongoService.db.collection('items').find(user).toArray(),
-  getItemCheck: (req) => mongoService.db.collection('items').findOne(req),
-  postMenuItem: (req) => mongoService.db.collection('items').insertOne(req),
+  getMenuItems: (menu) => mongoService.db.collection('items').find(menu).toArray(),
+  getItemCheck: (item) => mongoService.db.collection('items').findOne({ name: { $eq: item.name } }),
+  postMenuItem: (item) => mongoService.db.collection('items').insertOne(item),
 };
 
 module.exports = service;
